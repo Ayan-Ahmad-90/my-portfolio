6c5a4a281e51559b32367eb9283d61d041e0866b
Ayan Ahmad <chaudharyayan100@gmail.com>
2025-08-14 02:12:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-portfolio/', // यहाँ अपने repo का नाम डालें
-  plugins: [react()],
+    plugins: [react()],
+  base: '/my-portfolio/',
 })
