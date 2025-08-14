49c72fc5caefc0566ba763de7171de4ede4425e3
Ayan Ahmad <chaudharyayan100@gmail.com>
2025-08-15 01:24:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MainApp from "./App"; // Importing MainApp instead of App
 import "./index.css"; // Your global styles
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MainApp /> {/* Using MainApp here */}
+    <BrowserRouter basename="/my-portfolio">
+      <MainApp />
+    </BrowserRouter>
   </React.StrictMode>
 );
